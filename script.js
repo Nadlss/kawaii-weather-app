f5959bd966cc39e0d417cf58dc0bd0439d1c598f
Nadlss <58627148+Nadlss@users.noreply.github.com>
2020-03-08 15:28:14 +0000

    Added current temperature feature

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#current-condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = `${response.data.main.humidity} %`;
+  windElement.innerHTML = `${response.data.wind.speed} KmH`;
+}
+
+let apiKey = "655cc338645c52514e1df31b37348c78";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
 let now = new Date();
 
 let dayOfMonth = now.getDate();
